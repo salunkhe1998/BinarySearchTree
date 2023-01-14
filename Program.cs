50d8880da1e913f981ddde12e78b98e3dabb060b
Ashwini <salunkheashwini1998@gmail.com>
2023-01-14 21:21:49 +0530

    method calling

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace BinarySearchTree
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("***** Welcome to Binary Search Tree Program *****");
+
+        }
+
+    }
+}
