667921a94dab2b0049a8518dd7ac00e8b0949f5b
Ashwini <salunkheashwini1998@gmail.com>
2023-01-14 21:41:04 +0530

    Define Constructor

---
+++ Node.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    public class Node
+    {
+
+        public Node leftnode { get; set; }
+        public Node rightnode { get; set; }
+        public int data { get; set; }
+
+
+    }
+}
